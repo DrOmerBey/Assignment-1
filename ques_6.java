fd7a5ff0f897e299a092b684cae3befea6645032
DrOmerBey <80975583+DrOmerBey@users.noreply.github.com>
2021-03-22 11:26:24 +0300

    Add files via upload

---
+++ ques_6.java
@@ -0,0 +1,22 @@
+package day_2;
+
+public class ques_6 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("  ^ ");
+		System.out.println("  /\\ ");
+		System.out.println(" /  \\ ");
+		System.out.println("/    \\");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println(" |  |");
+		System.out.println("_______");
+
+	}
+
+}
